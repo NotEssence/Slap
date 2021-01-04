9bb1b8ce04265eefdf3b196d6118061b44e8ee64
Isham <Isham@Ishams-MacBook-Air.local>
2021-01-04 22:11:20 +0600

    central pile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import './App.css'
+
+function splitToChunks(array, parts) {
+  let result = []
+  for (let i = parts; i > 0; i--) {
+    result.push(array.splice(0, Math.ceil(array.length / i)))
+  }
+  return result
+}
+
+function PileItem(props) {
+  const color = (() => {
+    if (props.suit === 'Hearts' || props.suit === 'Diamonds') {
+      return '#ff6257'
+    } else {
+      return '#111827'
+    }
+  })()
+
+  return (
+    <div className="text-gray-50 bg-green-300 text-3xl font-bold m-3 rounded-md p-2 text-center">
+      <div className="m-2">
+        { props.kind } of
+        <div className="inline font-extrabold text-3xl ml-1 " style={{ color: color }}>
+          { props.suit }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Pile(props) {
+  return (
+    <ul className="h-screen w-1/5 overflow-hidden overflow-y-scroll bg-green-400">
+      { props.cards.slice().reverse().map((card, i) => <PileItem {...card} key={i} />) }
+    </ul>
+  )
+}
 
 function App() {
-  return (
-    <div>
+  const deck = [{ kind: "King", suit: "Clubs" }, { kind: "King", suit: "Hearts" }]
 
+  return (
+    <div className="flex">
+      <Pile cards={deck} />
     </div>
   )
 }
